5959b845681ecb8d5382e157d9ffa0c9f4c062da
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-11-13 16:19:09 +0100

    docs: add filter to show only published=true posts

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,12 +4,15 @@ import posts from '../data/posts';
 
 export default function Main() {
 
+    // logica
+    const updated_posts = posts.filter(post => post.published === true);
+
     // markup
     return (
         <main>
             <div className="container">
                 <div className="row">
-                    {posts.map(post => <PostCard key={post.id} post={post} />)}
+                    {updated_posts.map(post => <PostCard key={post.id} post={post} />)}
                 </div>
             </div>
         </main>
