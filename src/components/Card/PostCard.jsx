4be7e104e1bc647d2a4d89446f1228b49117a523
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-11-13 16:32:28 +0100

    docs: add tags section on card

--- src/components/Card/PostCard.jsx
+++ src/components/Card/PostCard.jsx
@@ -1,28 +1,33 @@
 // importazioni
 import style from '../Button/Button.module.css';
 
-export default function PostCard({post}) {
-    
+export default function PostCard({ post }) {
+
     // logica
     const button = 'leggi di più';
 
     // markup
     return (
-        <div className="card">
-            <div className="card-top">
-                <img src="https://picsum.photos/600/400" alt={post.title} />
+        <>
+            <div className="card">
+                <div className="card-top">
+                    <img src="https://picsum.photos/600/400" alt={post.title} />
+                </div>
+                <div className="card-bottom">
+                    <h4>
+                        {post.title}
+                    </h4>
+                    <p>
+                        {post.content}
+                    </p>
+                    <p className="tags">
+                        {post.tags.join(' ')}
+                    </p>
+                    <button className={style.btn}>
+                        {button.toUpperCase()}
+                    </button>
+                </div>
             </div>
-            <div className="card-bottom">
-                <h4>
-                    {post.title}
-                </h4>
-                <p>
-                    {post.content}
-                </p>
-                <button className={style.btn}>
-                    {button.toUpperCase()}
-                </button>
-            </div>
-        </div>
+        </>
     )
 }
